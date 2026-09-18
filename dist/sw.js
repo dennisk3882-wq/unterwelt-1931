8324158268c40374c062bc66ccfb012784f50f7a
dennisk3882-wq <dennisk3882@gmail.com>
2026-09-18 23:21:49 +0200

    Cache Empire Update and bump PWA revision 11

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='unterwelt-1931-v10',FILES=['./','index.html','style.css','rules.css','online.css','app.js','rules.js','online.js','menu.js','assets/map/unterwelt-city-map.webp','manifest.webmanifest','icon-192.png','icon-512.png'];
+const CACHE='unterwelt-1931-v11',FILES=['./','index.html','style.css','rules.css','online.css','empire.css','app.js','rules.js','online.js','menu.js','empire-loader.js','empire/part0.gz.b64','empire/part1.gz.b64','empire/part2.gz.b64','empire/part3.gz.b64','assets/map/unterwelt-city-map.webp','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('unterwelt-1931-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===self.location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
