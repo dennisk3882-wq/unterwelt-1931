aa314c8513c6450525bad123e22e2a01a7b7e584
dennisk3882-wq <dennisk3882@gmail.com>
2026-09-18 17:08:09 +0200

    Cache online client for PWA revision 4

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='unterwelt-1931-v3',FILES=['./','index.html','style.css','rules.css','app.js','rules.js','manifest.webmanifest','icon-192.png','icon-512.png'];
+const CACHE='unterwelt-1931-v4',FILES=['./','index.html','style.css','rules.css','online.css','app.js','rules.js','online.js','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('unterwelt-1931-')&&k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===self.location.origin)e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
