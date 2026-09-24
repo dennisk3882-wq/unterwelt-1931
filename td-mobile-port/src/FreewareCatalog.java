59c3d68c0f639d3f20ffd00c6eb6458591b360b9
dennisk3882-wq <dennisk3882@gmail.com>
2026-09-24 19:11:02 +0200

    Add Tiberian Dawn Android port source files

---
+++ td-mobile-port/src/FreewareCatalog.java
@@ -0,0 +1,20 @@
+package org.tiberiandawn.android;
+
+/** Metadata for the publicly distributed C&C Gold freeware ISO images. */
+public final class FreewareCatalog {
+    private FreewareCatalog() {}
+
+    public static final String GDI_PAGE =
+        "https://www.moddb.com/games/cc-gold/downloads/command-conquer-gold-free-game-gdi-iso";
+    public static final String NOD_PAGE =
+        "https://www.moddb.com/games/cc-gold/downloads/command-conquer-gold-free-game-nod-iso";
+
+    public static final String GDI_FILENAME = "CnC_GDI95.iso";
+    public static final String NOD_FILENAME = "CnC_NOD95.iso";
+
+    public static final long GDI_SIZE = 608_987_136L;
+    public static final long NOD_SIZE = 592_476_160L;
+
+    public static final String GDI_MD5 = "1cb7c6290718c5e729ae1a4a146aa793";
+    public static final String NOD_MD5 = "1dfb2ccae81117b053b60d70e8381250";
+}
