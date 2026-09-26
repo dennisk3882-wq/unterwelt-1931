badfa6fe20f4e6d465a9935d60c5d0dab1aa0310
dennisk3882-wq <dennisk3882@gmail.com>
2026-09-26 15:26:58 +0200

    Add automatic freeware download endpoints

--- td-mobile-port/src/FreewareCatalog.java
+++ td-mobile-port/src/FreewareCatalog.java
@@ -9,6 +9,14 @@ public final class FreewareCatalog {
     public static final String NOD_PAGE =
         "https://www.moddb.com/games/cc-gold/downloads/command-conquer-gold-free-game-nod-iso";
 
+    public static final int GDI_MODDB_FILE_ID = 110505;
+    public static final int NOD_MODDB_FILE_ID = 110507;
+
+    public static final String GDI_START =
+        "https://www.moddb.com/downloads/start/" + GDI_MODDB_FILE_ID;
+    public static final String NOD_START =
+        "https://www.moddb.com/downloads/start/" + NOD_MODDB_FILE_ID;
+
     public static final String GDI_FILENAME = "CnC_GDI95.iso";
     public static final String NOD_FILENAME = "CnC_NOD95.iso";
 
@@ -17,4 +25,6 @@ public final class FreewareCatalog {
 
     public static final String GDI_MD5 = "1cb7c6290718c5e729ae1a4a146aa793";
     public static final String NOD_MD5 = "1dfb2ccae81117b053b60d70e8381250";
+
+    public static final long TOTAL_DOWNLOAD_SIZE = GDI_SIZE + NOD_SIZE;
 }
